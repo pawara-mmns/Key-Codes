62a4d6de80f612ebe0f96862e1a4cc222916ebe0
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-27 11:55:09 +0530

    Enhance key event handling by updating DOM elements with key information

--- keycode.js
+++ keycode.js
@@ -1,9 +1,31 @@
 document.addEventListener("keyup", procesKeyEvent);
 
+const code = document.getElementById("code");
+const which = document.getElementById("which");
+const key = document.getElementById("key");
+const keyCode = document.getElementById("keyCode");
+const keyPressed = document.getElementById("key-pressed");
+
 function procesKeyEvent(e){
-    console.log(e);
+    // console.log(e);
 
     // if(e.key == "Enter"){
     //     document.body.style.backgroundColor = "lightcoral";
     // }
+    
+    if(e.code){
+        code.innerText = e.code;
+    }
+    if(e.which){
+        which.innerText = e.which;
+    }
+    if(e.key){
+        key.innerText = e.key;
+        keyPressed.innerText = e.key;
+        
+    }
+    if(e.code){
+        keyCode.innerText = e.keyCode;
+    }
+
 }
