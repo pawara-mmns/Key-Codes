8975316fd0c1507e178b9b05312cdc4519295131
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-27 11:21:05 +0530

    Add initial HTML structure and keycode event listener

---
+++ keycode.js
@@ -0,0 +1,9 @@
+document.addEventListener("keyup", procesKeyEvent);
+
+function procesKeyEvent(e){
+    console.log(e);
+
+    // if(e.key == "Enter"){
+    //     document.body.style.backgroundColor = "lightcoral";
+    // }
+}
